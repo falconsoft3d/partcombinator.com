800b9efed71f4a28e6227e97b84a6f287265edb4
Marlon Falcon <marlonfalcon@MBP-i5-16G-Home.technicolor.net>
2020-11-16 21:48:23 +0100

    Subiendo Modulos

--- apps/basecombinator/admin.py
+++ apps/basecombinator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (
+    PyCompany,PyCron,PyCountry)
 
 # Register your models here.
+admin.site.register(PyCompany)
+admin.site.register(PyCron)
+admin.site.register(PyCountry)
--- apps/basecombinator/models/__init__.py
+++ apps/basecombinator/models/__init__.py
@@ -1,2 +1,9 @@
 # Localfolder Library
 from .father import PyFather
+from .log import PyLog
+from .plugin import PyPlugin
+from .country import PyCountry
+from .cron import PyCron
+from .sequence import PySequence
+from . company import PyCompany
+from . base_config import BaseConfig
