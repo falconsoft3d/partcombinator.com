71f37f2bba98a12edffec85a1284195df93df727
Marlon Falcon <marlonfalcon@MBP-i5-16G-Home.technicolor.net>
2020-11-17 00:58:21 +0100

    Update Requirements

--- partcombinator/urls.py
+++ partcombinator/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path
+from django.views.static import serve
+from apps.basecombinator import views as basecombinator_views
 
 urlpatterns = [
+    path('', basecombinator_views.home, name="home"),
+    path('apps/', basecombinator_views.apps, name="apps"),
     path('admin/', admin.site.urls),
 ]
